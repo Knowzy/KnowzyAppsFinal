f7f222c008e6b4ce29d56e9263130cff147f5c71
stammen <dalestam@microsoft.com>
2017-06-07 08:24:10 -0700

    solution for 2.2.2

---
+++ stories/2/2.2.2/src/Microsoft.Knowzy.UWP/MainPage.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using Windows.UI.ViewManagement;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace Microsoft.Knowzy.UWP
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            ApplicationView.GetForCurrentView().TryResizeView(new Size(800, 800));
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs args)
+        {
+            if (args.Parameter != null)
+            {
+                WwwFormUrlDecoder decoder = new WwwFormUrlDecoder(args.Parameter.ToString());
+                try
+                {
+                    var message = decoder.GetFirstValueByName("nose");
+                    webView.Source = new Uri(message);
+                }
+                catch (Exception ex)
+                {
+                    System.Diagnostics.Debug.WriteLine("MainPage OnNavigatedTo Error: " + ex.Message);
+                }
+            }
+        }
+    }
+}
---
+++ stories/2/2.2.2/src/Microsoft.Knowzy.WPF/Views/MainView.xaml.cs
@@ -0,0 +1,55 @@
+﻿// ******************************************************************
+
+// Copyright (c) Microsoft. All rights reserved.
+// This code is licensed under the MIT License (MIT).
+// THE CODE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+// INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+// IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+// DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+// TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH
+// THE CODE OR THE USE OR OTHER DEALINGS IN THE CODE.
+
+// ******************************************************************
+using System;
+using System.Diagnostics;
+using System.IO;
+using Microsoft.Knowzy.UwpHelpers;
+
+namespace Microsoft.Knowzy.WPF.Views
+{
+    public partial class MainView
+    {
+        public MainView()
+        {
+            InitializeComponent();
+            if (ExecutionMode.IsRunningAsUwp())
+            {
+                try
+                {
+                    // get the path to the App folder (WPF or UWP).
+                    var path = AppFolders.Local;
+                    FileSystemWatcher watcher = new FileSystemWatcher(path);
+                    watcher.EnableRaisingEvents = true;
+                    watcher.Changed += Watcher_Changed;
+                }
+                catch (Exception ex)
+                {
+                    Debug.WriteLine("FileSystemWatcher Error:" + ex.Message);
+                }
+            }
+        }
+
+        private void Watcher_Changed(object sender, FileSystemEventArgs e)
+        {
+            if (ExecutionMode.IsRunningAsUwp())
+            {
+                if (File.Exists(e.FullPath))
+                {
+                    var xml = "<toast><visual><binding template='ToastGeneric'><image src='" + e.FullPath + "'/><text hint-maxLines='1'>Microsoft.Knowzy.WPF received a new image</text></binding></visual></toast>";
+                    Toast.CreateToast(xml);
+                }
+            }
+        }
+    }
+}
